62e7c05e2b2915e740d8ce90f0acef1f9e658ab5
Hong Gu <macrogu@qq.com>
2023-04-13 16:30:44 +0800

    add support for token of WebSocket

--- WebSocketServer/server.py
+++ WebSocketServer/server.py
@@ -1,15 +1,29 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+from urllib.parse import urlparse, parse_qs
+import re
+
+
 
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
         # echo message back to client
         print("receive data: " + self.data)
-        self.sendMessage(self.data + " From Server")
+        self.sendMessage(self.data + " From Server  " + self.request )
 
     def handleConnected(self):
-        print(self.address, 'connected')
+        #print(self.client, 'connected  ' )
+        pass
 
+    def handleConnectedWithHeader(self, header):
+        match = re.search(r"token=([\w\d\S]+)\s", header.decode('utf-8'))
+        token = match.group(1)
+        if token == "thisisavalidtoken" :
+            print("connected")
+        else:
+            print("not connected")
+            raise Exception("token is not valid")
+    
     def handleClose(self):
         print(self.address, 'closed')
 
