e5e8f500a37c9a8d10c60dcb6f4f0cd8d31354dc
Hong Gu <macrogu@qq.com>
2023-04-13 10:54:04 +0800

    add RestfulAPI_WebServer

---
+++ RestfulAPI_WebServer/mars4api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import include, path
+from rest_framework import routers
+from mars4api.app_auth import views
+
+
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+        
+]
