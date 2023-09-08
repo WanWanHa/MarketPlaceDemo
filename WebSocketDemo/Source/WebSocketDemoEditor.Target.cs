18c5fb61598e26e5178136d653bc6a2080c66d38
Hong <guhong.usa@gmail.com>
2023-09-08 22:38:00 +0800

    update to 5.3

--- WebSocketDemo/Source/WebSocketDemoEditor.Target.cs
+++ WebSocketDemo/Source/WebSocketDemoEditor.Target.cs
@@ -9,7 +9,7 @@ public class WebSocketDemoEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("WebSocketDemo");
 	}
 }
