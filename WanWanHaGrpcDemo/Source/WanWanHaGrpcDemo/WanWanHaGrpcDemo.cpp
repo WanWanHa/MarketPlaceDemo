ed2fc91195fa242df030a9711b5f6e01907c129f
Hong <guhong.usa@gmail.com>
2023-10-29 01:39:53 +0800

    添加 grpc demo

---
+++ WanWanHaGrpcDemo/Source/WanWanHaGrpcDemo/WanWanHaGrpcDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2023, Hong Gu <guhong.usa@gmail.com>. All Rights Reserved.
+
+#include "WanWanHaGrpcDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WanWanHaGrpcDemo, "WanWanHaGrpcDemo" );
---
+++ WanWanHaGrpcDemo/Source/WanWanHaGrpcDemoEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2023, Hong Gu <guhong.usa@gmail.com>. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class WanWanHaGrpcDemoEditorTarget : TargetRules
+{
+	public WanWanHaGrpcDemoEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("WanWanHaGrpcDemo");
+	}
+}
