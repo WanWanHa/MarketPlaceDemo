1f3ca334be540be43e396d1057349c15dc71b363
Hong <guhong.usa@gmail.com>
2024-01-06 16:44:39 +0800

    添加 5.3 版本

---
+++ VehiclePhysNetworkDemo/Source/VehiclePhysNetworkDemo/CustomVehiclePawn.cpp
@@ -0,0 +1,290 @@
+// Copyright (c) 2024, Hong Gu <guhong.usa@gmail.com>. All Rights Reserved.
+
+
+#include "CustomVehiclePawn.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "ChaosWheeledVehicleMovementComponent.h"
+#include "Engine/SkeletalMesh.h"
+#include "Engine/Engine.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Components/TextRenderComponent.h"
+#include "Materials/Material.h"
+#include "GameFramework/Controller.h"
+#include "VehiclePhysNetworkMovementComponent.h"
+
+#ifndef HMD_MODULE_INCLUDED
+#define HMD_MODULE_INCLUDED 0
+#endif
+
+// Needed for VR Headset
+#if HMD_MODULE_INCLUDED
+#include "IXRTrackingSystem.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#endif // HMD_MODULE_INCLUDED
+
+const FName ACustomVehiclePawn::LookUpBinding("LookUp");
+const FName ACustomVehiclePawn::LookRightBinding("LookRight");
+
+#define LOCTEXT_NAMESPACE "VehiclePawn"
+
+
+
+ACustomVehiclePawn::ACustomVehiclePawn(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer.SetDefaultSubobjectClass<UVehiclePhysNetworkMovementComponent>(VehicleMovementComponentName))
+{
+
+	//return;
+	// Car mesh
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh> CarMesh(TEXT("/Game/Vehicle/Sedan/Sedan_SkelMesh.Sedan_SkelMesh"));
+	GetMesh()->SetSkeletalMesh(CarMesh.Object);
+
+	static ConstructorHelpers::FClassFinder<UObject> AnimBPClass(TEXT("/Game/Vehicle/Sedan/Sedan_AnimBP"));
+	GetMesh()->SetAnimInstanceClass(AnimBPClass.Class);
+	
+	// Create a spring arm component
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->TargetOffset = FVector(0.f, 0.f, 200.f);
+	SpringArm->SetRelativeRotation(FRotator(-15.f, 0.f, 0.f));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 600.0f;
+	SpringArm->bEnableCameraRotationLag = true;
+	SpringArm->CameraRotationLagSpeed = 7.f;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+
+	// Create camera component 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+	Camera->FieldOfView = 90.f;
+
+	// Create In-Car camera component 
+	InternalCameraOrigin = FVector(0.0f, -40.0f, 120.0f);
+
+	InternalCameraBase = CreateDefaultSubobject<USceneComponent>(TEXT("InternalCameraBase"));
+	InternalCameraBase->SetRelativeLocation(InternalCameraOrigin);
+	InternalCameraBase->SetupAttachment(GetMesh());
+
+	InternalCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("InternalCamera"));
+	InternalCamera->bUsePawnControlRotation = false;
+	InternalCamera->FieldOfView = 90.f;
+	InternalCamera->SetupAttachment(InternalCameraBase);
+
+	//Setup TextRenderMaterial
+	static ConstructorHelpers::FObjectFinder<UMaterial> TextMaterial(TEXT("Material'/Engine/EngineMaterials/AntiAliasedTextMaterialTranslucent.AntiAliasedTextMaterialTranslucent'"));
+	
+	UMaterialInterface* Material = TextMaterial.Object;
+
+	// Create text render component for in car speed display
+	InCarSpeed = CreateDefaultSubobject<UTextRenderComponent>(TEXT("IncarSpeed"));
+	InCarSpeed->SetTextMaterial(Material);
+	InCarSpeed->SetRelativeLocation(FVector(70.0f, -75.0f, 99.0f));
+	InCarSpeed->SetRelativeRotation(FRotator(18.0f, 180.0f, 0.0f));
+	InCarSpeed->SetupAttachment(GetMesh());
+	InCarSpeed->SetRelativeScale3D(FVector(1.0f, 0.4f, 0.4f));
+
+	// Create text render component for in car gear display
+	InCarGear = CreateDefaultSubobject<UTextRenderComponent>(TEXT("IncarGear"));
+	InCarGear->SetTextMaterial(Material);
+	InCarGear->SetRelativeLocation(FVector(66.0f, -9.0f, 95.0f));	
+	InCarGear->SetRelativeRotation(FRotator(25.0f, 180.0f,0.0f));
+	InCarGear->SetRelativeScale3D(FVector(1.0f, 0.4f, 0.4f));
+	InCarGear->SetupAttachment(GetMesh());
+	
+	// Colors for the incar gear display. One for normal one for reverse
+	GearDisplayReverseColor = FColor(255, 0, 0, 255);
+	GearDisplayColor = FColor(255, 255, 255, 255);
+
+	// Colors for the in-car gear display. One for normal one for reverse
+	GearDisplayReverseColor = FColor(255, 0, 0, 255);
+	GearDisplayColor = FColor(255, 255, 255, 255);
+
+	bInReverseGear = false;
+
+	SetReplicatingMovement(true);
+}
+
+void ACustomVehiclePawn::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// set up gameplay key bindings
+	check(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ACustomVehiclePawn::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ACustomVehiclePawn::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp");
+	PlayerInputComponent->BindAxis("LookRight");
+
+	PlayerInputComponent->BindAction("Handbrake", IE_Pressed, this, &ACustomVehiclePawn::OnHandbrakePressed);
+	PlayerInputComponent->BindAction("Handbrake", IE_Released, this, &ACustomVehiclePawn::OnHandbrakeReleased);
+	PlayerInputComponent->BindAction("SwitchCamera", IE_Pressed, this, &ACustomVehiclePawn::OnToggleCamera);
+
+}
+
+void ACustomVehiclePawn::MoveForward(float Val)
+{
+	if (!GetVehicleMovementComponent())
+	{
+		return;
+	}
+	GetVehicleMovementComponent()->SetThrottleInput(Val);
+}
+
+void ACustomVehiclePawn::MoveRight(float Val)
+{
+	if (!GetVehicleMovementComponent())
+	{
+		return;
+	}
+	GetVehicleMovementComponent()->SetSteeringInput(Val);
+}
+
+void ACustomVehiclePawn::OnHandbrakePressed()
+{
+	if (!GetVehicleMovementComponent())
+	{
+		return;
+	}
+	GetVehicleMovementComponent()->SetHandbrakeInput(true);
+}
+
+void ACustomVehiclePawn::OnHandbrakeReleased()
+{
+	if (!GetVehicleMovementComponent())
+	{
+		return;
+	}
+	GetVehicleMovementComponent()->SetHandbrakeInput(false);
+}
+
+void ACustomVehiclePawn::OnToggleCamera()
+{
+	EnableIncarView(!bInCarCameraActive);
+}
+
+void ACustomVehiclePawn::EnableIncarView(const bool bState, const bool bForce)
+{
+	if (((bState != bInCarCameraActive) || ( bForce == true )) && Camera)
+	{
+		bInCarCameraActive = bState;
+		
+		if (bState == true)
+		{
+			Camera->Deactivate();
+			InternalCamera->Activate();
+		}
+		else
+		{
+			InternalCamera->Deactivate();
+			Camera->Activate();
+		}
+		
+		InCarSpeed->SetVisibility(bInCarCameraActive);
+		InCarGear->SetVisibility(bInCarCameraActive);
+	}
+}
+
+void ACustomVehiclePawn::Tick(float Delta)
+{
+	Super::Tick(Delta);
+
+	// Setup the flag to say we are in reverse gear
+	bInReverseGear = GetVehicleMovement()->GetCurrentGear() < 0;
+	
+	// Update the strings used in the hud (incar and onscreen)
+	UpdateHUDStrings();
+
+	// Set the string in the incar hud
+	SetupInCarHUD();
+
+	bool bHMDActive = false;
+#if HMD_MODULE_INCLUDED
+	if ((GEngine->XRSystem.IsValid() == true) && ((GEngine->XRSystem->IsHeadTrackingAllowed() == true) || (GEngine->IsStereoscopic3D() == true)))
+	{
+		bHMDActive = true;
+	}
+#endif // HMD_MODULE_INCLUDED
+	if (bHMDActive == false)
+	{
+		if ( (InputComponent) && (bInCarCameraActive == true ))
+		{
+			FRotator HeadRotation = InternalCamera->GetRelativeRotation();
+			HeadRotation.Pitch += InputComponent->GetAxisValue(LookUpBinding);
+			HeadRotation.Yaw += InputComponent->GetAxisValue(LookRightBinding);
+			InternalCamera->SetRelativeRotation(HeadRotation);
+		}
+	}
+}
+
+void ACustomVehiclePawn::BeginPlay()
+{
+	Super::BeginPlay();
+	bool bEnableInCar = false;
+#if HMD_MODULE_INCLUDED
+	bEnableInCar = UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled();
+#endif // HMD_MODULE_INCLUDED
+	EnableIncarView(bEnableInCar,true);
+}
+
+void ACustomVehiclePawn::OnRep_ReplicatedMovement()
+{
+	//NetVehicleMovement->bNetworkUpdateReceived = true;
+
+
+	Super::OnRep_ReplicatedMovement();
+}
+
+void ACustomVehiclePawn::PreReplication(IRepChangedPropertyTracker& ChangedPropertyTracker)
+{
+	Super::PreReplication(ChangedPropertyTracker);
+}
+
+void ACustomVehiclePawn::PostNetReceiveLocationAndRotation()
+{
+	
+}
+
+void ACustomVehiclePawn::UpdateHUDStrings()
+{
+	float KPH = FMath::Abs(GetVehicleMovement()->GetForwardSpeed()) * 0.036f;
+	int32 KPH_int = FMath::FloorToInt(KPH);
+
+	// Using FText because this is display text that should be localizable
+	SpeedDisplayString = FText::Format(LOCTEXT("SpeedFormat", "{0} km/h"), FText::AsNumber(KPH_int));
+	
+	if (bInReverseGear == true)
+	{
+		GearDisplayString = FText(LOCTEXT("ReverseGear", "R"));
+	}
+	else
+	{
+		int32 Gear = GetVehicleMovement()->GetCurrentGear();
+		GearDisplayString = (Gear == 0) ? LOCTEXT("N", "N") : FText::AsNumber(Gear);
+	}	
+}
+
+void ACustomVehiclePawn::SetupInCarHUD()
+{
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if ((PlayerController != nullptr) && (InCarSpeed != nullptr) && (InCarGear != nullptr) )
+	{
+		// Setup the text render component strings
+		InCarSpeed->SetText(SpeedDisplayString);
+		InCarGear->SetText(GearDisplayString);
+		
+		if (bInReverseGear == false)
+		{
+			InCarGear->SetTextRenderColor(GearDisplayColor);
+		}
+		else
+		{
+			InCarGear->SetTextRenderColor(GearDisplayReverseColor);
+		}
+	}
+}
+
+#undef LOCTEXT_NAMESPACE
