0165c2c17046d6b647e9f43ccb57550072470482
Hong <guhong.usa@gmail.com>
2024-03-27 10:24:04 +0800

    使用新的版权声明

--- RestfulAPIDemo/RestfulAPIDemo/Source/RestfulAPIDemo/RestfulAPIDemoGameMode.cpp
+++ RestfulAPIDemo/RestfulAPIDemo/Source/RestfulAPIDemo/RestfulAPIDemoGameMode.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2022, Hong Gu <guhong.usa@gmail.com>. All Rights Reserved.
+// Copyright WanWanHa NetWork Technology, LLC. All Rights Reserved.
 
 #include "RestfulAPIDemoGameMode.h"
 #include "UObject/ConstructorHelpers.h"
