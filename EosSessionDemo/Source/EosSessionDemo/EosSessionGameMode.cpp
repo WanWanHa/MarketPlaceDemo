ca41d2835786c1b51b192cd3c9531346cc9fb9bc
Hong <macrogu@qq.com>
2024-04-02 10:27:27 +0800

    open level

--- EosSessionDemo/Source/EosSessionDemo/EosSessionGameMode.cpp
+++ EosSessionDemo/Source/EosSessionDemo/EosSessionGameMode.cpp
@@ -9,14 +9,14 @@ void AEosSessionGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer);
 
-	int32 NumOfPlayers = GameState.Get()->PlayerArray.Num();
-	if (NumOfPlayers >= 2)
-	{
-		UWorld* World = GetWorld();
-		if (World)
-		{
-			bUseSeamlessTravel = true;
-			World->ServerTravel(FString("/Game/ThirdPerson/Maps/Lobby?listen"));
-		}
-	}
+	//int32 NumOfPlayers = GameState.Get()->PlayerArray.Num();
+	//if (NumOfPlayers >= 2)
+	//{
+	//	UWorld* World = GetWorld();
+	//	if (World)
+	//	{
+	//		bUseSeamlessTravel = true;
+	//		World->ServerTravel(FString("/Game/ThirdPerson/Maps/Lobby?listen"));
+	//	}
+	//}
 }
