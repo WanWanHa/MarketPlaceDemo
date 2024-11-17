eb0b610db1dae1d5a1e6045c1a2a30cd57596568
Hong <macrogu@qq.com>
2024-11-17 13:32:33 +0800

    add demo for 5.5

---
+++ DataBaseDemo/PostgreSqlDriverDemo/Source/PostgreSqlDriverDemo/PostgreSqlDriverDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright 2024 (c) WanWanHa, LLC. All Rights Reserved.
+
+#include "PostgreSqlDriverDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PostgreSqlDriverDemo, "PostgreSqlDriverDemo" );
---
+++ DataBaseDemo/PostgreSqlDriverDemo/Source/PostgreSqlDriverDemo/PostgreSqlDriverDemoGameModeBase.h
@@ -0,0 +1,20 @@
+// Copyright 2024 (c) WanWanHa, LLC. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PostgreSqlDriverDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POSTGRESQLDRIVERDEMO_API APostgreSqlDriverDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+
+public:
+	virtual void BeginPlay() override;
+};
